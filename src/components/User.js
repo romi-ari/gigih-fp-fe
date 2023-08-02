9647da2a8c8e7ee05aebc350087da27ac4ba5b40
romiari <romiari777@gmail.com>
2023-08-02 20:22:06 +0700

    add login spotify

--- src/components/User.js
+++ src/components/User.js
@@ -2,12 +2,28 @@
 
 import React from 'react'
 
+const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+const redirect_uri = "http://localhost:3000/";
+const scopes = ["user-read-private", "user-read-email", "user-modify-playback-state"];
+
 function User() {
+  
+    function handleLogin() {
+        const authEndpoint = "https://accounts.spotify.com/authorize";
+        const queryParams = new URLSearchParams({
+          client_id,
+          response_type: "token",
+          redirect_uri,
+          scope: scopes.join(" "),
+        })
+        window.location = `${authEndpoint}?${queryParams}`
+    }
+
     return (
         <div className="flex items-center justify-end sticky top-0 pt-4 bg-opacity-0 bg-neutral-950">
             <div className="flex items-center gap-4">
-                <button type="button" className="rounded-full font-bold w-[111px] h-10 hover:bg-gray-700 bg-neutral-950">
-                  <span className="text-white text-xl font-medium">Sign In</span>
+                <button onClick={handleLogin} type="button" className="rounded-full font-bold w-[111px] h-10 hover:bg-gray-700 bg-neutral-950">
+                    <span className="text-white text-xl font-medium">Sign In</span>
                 </button>
                 <button type="button" className="w-10 h-10 text-m  rounded-full md:mr-0 dark:hover:bg-gray-700 bg-neutral-950">
                     <span className="sr-only">Open user menu</span>
